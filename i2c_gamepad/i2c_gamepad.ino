063cd96e6cdb032883a9b467b61df0982e528d57
piloChambert <pierrelaurent.chambert@gmail.com>
2015-09-20 13:44:23 +0200

    Updated arduino code to handle switch

--- i2c_gamepad/i2c_gamepad.ino
+++ i2c_gamepad/i2c_gamepad.ino
@@ -7,7 +7,18 @@
 
 typedef struct {
   uint16_t buttons; // button status
-} I2CGamepadStatus;
+  uint16_t axis0; // first axis
+  uint16_t axis1; // second axis
+} I2CJoystickStatus;
+
+I2CJoystickStatus status;
+
+int state = HIGH;      // the current state of the output pin
+int reading;           // the current reading from the input pin
+int previous = LOW;    // the previous reading from the input pin
+
+long time = 0;         // the last time the output pin was toggled
+long debounce = 200;   // the debounce time, increase if the output flickers
 
 void setup()
 {
@@ -20,23 +31,62 @@ void setup()
     ; // wait for serial port to connect. Needed for Leonardo only
   }
   Serial.println("I2C Gamepad controller");
+
+  // default status
+  status.buttons = 0;
+  status.axis0 = 127;
+  status.axis1 = 127;
+
+  // pin configuration
+  pinMode(2, INPUT);
 }
 
 void loop()
 {
-  delay(1000);
+  reading = digitalRead(2);
+
+  /*
+  if(previous != reading) {
+    if(reading == HIGH) {
+      Serial.println("HIGH");
+    } else {
+      Serial.println("LOW");
+    }
+  }*/
+
+  previous = reading;
+
+  if(reading == HIGH) {
+    status.buttons &= ~0x01;
+  } else {
+    status.buttons |= 0x01;
+  }
+  
+  // if the input just went from LOW and HIGH and we've waited long enough
+  // to ignore any noise on the circuit, toggle the output pin and remember
+  // the time
+  /*
+  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
+    if (state == HIGH) {
+      state = LOW;
+      Serial.println("LOW");
+    }
+    else {
+      state = HIGH;
+      Serial.println("HIGH");
+    }
+
+    time = millis();    
+  }
+
+  previous = reading;*/
+  
+  //delay(1000);
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent()
 {
-  static int i = 16;
-
-  I2CGamepadStatus status;
-  status.buttons = 16 + (i % 2);
-
-  Wire.write((char *)&status, sizeof(I2CGamepadStatus)); 
-  
-  i++;
+  Wire.write((char *)&status, sizeof(I2CJoystickStatus)); 
 }
